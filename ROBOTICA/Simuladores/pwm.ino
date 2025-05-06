c30c20b852da0c8d1f7906827e58bdca16466c32
lucas-marin-123 <luquinmarin123@gmail.com>
2025-05-06 14:06:25 -0300

    actualizando repo

---
+++ ROBOTICA/Simuladores/pwm.ino
@@ -0,0 +1,20 @@
+1 int blueLed 97 
+2 int brightness1 =0; 
+3 int brightness2 #125; 
+int brightness3 #255; 
+5 int dt = 500; 
+6 
+7 void setup() { 
+a // put your setup code here, to run once: 
+pinMode(blueled, OUTPUT); 
+101 
+11 
+12 void loop() { 
+13 // put your main code here, to zun repeatedly 
+14 analogWrite(blueled, brightnessl); 
+15 delay(dt); 
+analogWrite(blueled, brightness2); 
+17 delay(dt); 
+18 analogWrite(blueLed, brightness3); 
+19 delay(dt): 
+201
