23d7036d0137d1404047ca2cda8171d0a1a8a13d
lucas-marin-123 <luquinmarin123@gmail.com>
2025-05-20 14:48:47 -0300

    actualizando repo

--- ROBOTICA/Simuladores/ejercicio_11/ejercicio_11.ino
+++ ROBOTICA/Simuladores/ejercicio_11/ejercicio_11.ino
@@ -6,7 +6,7 @@ Serial.begin(9600);
 } 
 void loop() { 
 // put your main code here, to run repeatedly: 
-Serial.println("i = "); 
+Serial.println("i =i+1"); 
 Serial.println(i); 
 delay(wait); 
 i=i+1; 
